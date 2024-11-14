925a0e7df692f76eb885e5381aa26c6fa1b509c4
saurabh-hs <shshinde@mitaoe.ac.in>
2024-11-14 22:21:25 +0530

    second commit

---
+++ Day2/index.js
@@ -0,0 +1,64 @@
+// Variables & Data types 
+
+// var a = 10;
+
+// console.log(a);
+
+// function example(){
+//     var x = 10;
+//     console.log(x);
+// }
+
+// example();
+// console.log(x);
+
+// var x = 10;
+// console.log(x);
+
+// x = "Mayank";
+// console.log(x);
+
+// let x = 10;
+
+// console.log(x);
+
+// function example() {
+//     let x = 13;
+//     console.log(x);
+// }
+
+// example();
+// console.log(x); //error
+
+// let x = 10;
+
+// function example() {
+//     let y = "Saurabh";
+//     console.log(x);
+//     console.log(y);
+// }
+
+// example();
+
+// let x = 10;
+// console.log(x);
+
+// x = "Saurabh"
+// console.log(x)
+
+// const x = 10;
+// console.log(x);
+
+// x = 20
+// console.log(x);  /error
+
+// const x = 2;
+
+function example() {
+    // console.log(x);
+
+    x = 10;  //error
+    console.log(x);
+}
+
+example();
