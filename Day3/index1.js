f2e6840eab8b1b1c04712250372be264143b84f6
saurabh-hs <shshinde@mitaoe.ac.in>
2024-11-15 11:04:33 +0530

    third commit

---
+++ Day3/index1.js
@@ -0,0 +1,105 @@
+//Operators & conditional statements
+
+// let number1 = 24;
+// let number2 = 10;
+// let number3 = 12.5;
+
+// let sum = number1 + number2 + number3;
+// console.log(sum);
+
+// let x = 10;
+// let y = 2;
+// console.log(x*y);
+
+// let a = 15;
+// let b = 5;
+// console.log(a/b);
+
+// console.log(a%b);
+
+// let a = 20;
+
+// a +=12;
+// a-=10;
+// a*=15;
+// a /= 2;
+// a %= 3;
+// console.log(a);
+
+// let a = 20;
+// let b = ++a;
+
+// let b = a++;
+// let b; 
+// console.log(b); //undefined
+
+// b = --a;
+// b = a--;
+// console.log(b);
+
+// let x = 10;
+
+// x = "Saurabh";
+// x = null;
+// x = undefined;
+// x = true;
+// x = 12.7658;
+
+// console.log(typeof(x));
+
+// let alert1 = true;
+// let alert2 = false;
+// let alert3 = true;
+
+// alert3 = false;
+
+// console.log(alert1 && alert2);
+// console.log(alert1 && alert3);
+// console.log(alert2 && alert3);
+
+// let num1 = 0;
+// let num2 = 1;
+// let num3 = 1;
+
+// console.log(num1 && num2);
+// console.log(num2 && num3);
+// console.log(num1 && num3);
+
+// console.log(alert1 || alert2);
+// console.log(alert2 || alert3);
+// console.log(alert1 || alert3);
+
+// let alert1 = false;
+// let alert2;
+// let alert3 = NaN;
+// let mySum = 45;
+// let gone = false;
+// let isFirst = true;
+
+// console.log(!alert1); //true
+// console.log(!alert2); //true
+// console.log(!alert3); //true
+// console.log(!mySum); //false    
+// console.log(!gone); //true
+// console.log(isFirst); //true
+
+// let num1 = 5;
+// let num2 = 7;
+
+// console.log(num1 & num2); //5
+
+// console.log(num1 | num2); //7
+
+// console.log(num1 ^ num2); //2
+
+// console.log(~num1); //-6
+
+// let a = 10;
+
+// console.log(a << 2); //40
+
+// console.log(a >> 2); //2
+
+let age = 9;
+
+(age >= 18) ? console.log('Can vote') : console.log('Cannot vote');
---
+++ Day3/index2.js
@@ -0,0 +1,56 @@
+//Conditional statement & switch case
+
+// let age = 20;
+
+// if(age >= 21) {
+//     console.log("You can drive");
+// }else{
+//     console.log("You cannot drive");
+// }
+
+// let num = 5;
+
+// if(num == 1) {
+//     console.log('A');
+// }else if(num == 2) {
+//     console.log('B');
+// }else if(num == 3) {
+//     console.log('C');
+// }else if(num == 4) {
+//     console.log('D');
+// }else{
+//     console.log('E');
+// }
+
+// let weight = 45;
+
+// if(weight > 70) {
+//     console.log("You are Overweight");
+// }else if(weight > 50 && weight <= 70) {
+//     console.log("You are Fit");
+// }else{
+//     console.log("You are Underweight");
+// }
+
+let num = 10;
+
+switch(num) {
+    case 1:
+        console.log('A');
+        break;
+
+    case 2:
+        console.log('B');
+        break;
+
+    case 3:
+        console.log('C');
+        break;
+
+    case 4:
+        console.log('D');
+        break;
+
+    default:
+        console.log('Z');
+}
