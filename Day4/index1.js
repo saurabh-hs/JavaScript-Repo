dd93528859e7163d232107a3d1e0837d6b1ca58e
saurabh-hs <shshinde@mitaoe.ac.in>
2024-11-16 12:39:06 +0530

    fourth commit

---
+++ Day4/index1.js
@@ -0,0 +1,33 @@
+// Loops
+
+// for(let i = 1; i < 6; i++) {
+//     console.log(i);
+// }
+
+// let i = 1;
+// while(i < 6) {
+//     console.log(i);
+//     i++;
+// }
+
+// let i =1;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i < 6);
+
+// for(let i = 5; i > 0; i--) {
+//     console.log(i);
+// }
+
+// let i = 5;
+// while(i > 0) {
+//     console.log(i);
+//     i--;
+// }
+
+// let i = 5;
+// do{
+//     console.log(i);
+//     i--;
+// }while(i > 0);
---
+++ Day4/index2.js
@@ -0,0 +1,34 @@
+// Strings & Operations
+
+// let firstName = "Saurabh ";
+// let secondName = "Shinde";
+
+// console.log(firstName+secondName);
+
+// let name = `Saurabh
+// Shinde this is
+// my
+// name`;
+
+// console.log(name);
+
+// let Name = new String("Saurabh Shinde");
+// let Name = new String('Saurabh Shinde');
+// console.log(Name);
+
+let opt1 = 'English';
+let opt2 = 'MARATHI';
+
+// console.log(opt2.length);
+console.log(opt1.toLocaleUpperCase());
+console.log(opt2.toLocaleLowerCase());
+console.log(opt2.substring(1, 4));
+
+console.log(opt1.substring(3));
+
+let sentence = "Hello \"World\" this is javascript language";
+console.log(sentence.split(' '));
+
+let words = sentence.split(' ');
+
+console.log(words.join(','));
