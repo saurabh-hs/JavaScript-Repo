1e7cc889e24867db65d1238c110c1c279936f11e
saurabh-hs <shshinde@mitaoe.ac.in>
2024-11-18 20:58:22 +0530

    seventh commit

---
+++ Day7/index1.js
@@ -0,0 +1,77 @@
+//Variable & function hositing
+
+// greet();
+
+// function greet() {
+//     console.log("Goode morning!");
+// }
+
+// console.log(a); //undefined
+
+// var a = 10;
+
+// console.log(value); //ReferenceError
+
+// let value = 20;
+
+// console.log(constValue); //ReferenceError
+
+// const constValue = 10;
+
+// console.log(greet()); //ReferenceError
+
+// let obj = {
+//     age:25,
+//     wt: 65,
+//     ht: 180,
+//     greet: () => {
+//         console.log("Hello world!");
+//     }
+// }
+
+// console.log(obj.age);
+
+// const arr = [
+//     function(a, b) {
+//         return a+b;
+//     },
+
+//     function(a, b) {
+//         return a-b;
+//     },
+
+//     function(a, b) {
+//         return a * b;
+//     }
+// ];
+
+// let first = arr[2];
+// let ans = first(10, 2);
+// console.log(ans);
+
+// function solve() {
+//     return function(number) {
+//         return number*number;
+//     }
+// }
+
+// let ans = solve();
+// let finalAns = ans(5);
+// console.log(finalAns);
+
+// function greetMe(greet, fullName) {
+//     console.log("Hello ", fullName);
+//     greet();
+// }
+
+// function greet() {
+//     console.log("Greeting for the day");
+// }
+
+// greetMe(greet, 'Saurabh Shinde');
+
+// const obj = new Human(); //ReferenceError
+
+// class Human{
+
+// }
---
+++ Day7/index2.js
@@ -0,0 +1,52 @@
+//Temporal Dead zone
+
+// console.log(marks);
+// console.log('Saurabh');
+// console.log('Shinde');
+// const marks = 100;
+// console.log(marks);
+
+// {
+//     const age = 100;
+// }
+
+// console.log(age);
+
+// console.log(height);
+// {
+//     var height = 180;
+// }
+
+// console.log(height);
+
+// function sayHello() {
+//     const fullName = "Saurabh";
+//     console.log("Hello world!", fullName);
+// }
+
+// console.log(fullName);
+// sayHello();
+
+//global scope
+// const age = 12;
+
+// console.log(age);
+
+// {
+//     console.log(age);
+// }
+
+// if(true) {
+//     console.log(age);
+// }
+
+// for(let i = 0; i < 2; i++) {
+//     console.log(age);
+// }
+
+// function sayHello() {
+//     console.log("hi", age);
+// }
+
+// sayHello();
+
